59b930815f954cd162877f7bd2d5ec1c9a2ef146
Louk Rath <loukrath@me.com>
2023-12-22 00:54:09 +0700

    fixed base url

--- utils/api.ts
+++ utils/api.ts
@@ -6,19 +6,19 @@ import axios from "@/libs/axios"
 const baseUrl = process.env.apiUrl;
 
 export const getAllTodos = async (): Promise<ITask[]> => {
-  const { data } = await axios.get(`${baseUrl}/todo`);
+  const { data } = await axios.get(`https://api.todo.livecmt.com/api/todo`);
 
   return data;
 }
 
 export const addNewTask = async (task: { todo: string }): Promise<AxiosResponse<ITask>> => {
-  return await axios.post(`${baseUrl}/todo`, task);
+  return await axios.post(`https://api.todo.livecmt.com/api/todo`, task);
 }
 
 export const updateTask = async (task: ITask): Promise<AxiosResponse<ITask>> => {
-  return await axios.put(`${baseUrl}/todo/${task.id}`, task);
+  return await axios.put(`https://api.todo.livecmt.com/api/todo/${task.id}`, task);
 }
 
 export const deleteTask = async (id: string): Promise<void> => {
-  await axios.delete(`${baseUrl}/todo/${id}`);
+  await axios.delete(`https://api.todo.livecmt.com/api/todo/${id}`);
 }
